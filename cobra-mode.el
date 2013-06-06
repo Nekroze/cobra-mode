66a891abb0afca62defee417ffa28f4d7d58ff24
Taylor "Nekroze" Lawson <nekroze@eturnilnetwork.com>
2013-06-06 22:07:25 +1000

    Removed compilation command and added more keywords and types

--- cobra-mode.el
+++ cobra-mode.el
@@ -6,51 +6,25 @@
 
 (add-to-list 'auto-mode-alist '("\\.cobra\\'" . cobra-mode))
 
-
-(defun cobra-compile ()
-  "Compile the file via Cobra."
-  (interactive)
-  (let ((cy-buffer (current-buffer)))
-    (with-current-buffer
-        (compile compile-command)
-      (set (make-local-variable 'cobra-buffer) cy-buffer)
-      (add-to-list (make-local-variable 'compilation-finish-functions)
-                   'cobra-compilation-finish)))
-  )
-
-(defun cobra-compilation-finish (buffer how)
-  "Called when Cobra compilation finishes."
-  ;; XXX could annotate source here
-  )
-
-(defvar cobra-mode-map
-  (let ((map (make-sparse-keymap)))
-    ;; Will inherit from `python-mode-map' thanks to define-derived-mode.
-    (define-key map "\C-c\C-c" 'cobra-compile)
-    map)
-  "Keymap used in `cobra-mode'.")
-
 (defvar cobra-font-lock-keywords
-  `(;; new keywords in Cobra language
-    (,(regexp-opt '("as" "require" "ensure" "body" "test" "using" "namespace"
-                    "get" "set" "throw") 'words)
+  `(
+
+    (,(regexp-opt '("as" "require" "ensure" "body" "test" "use" "catch" "ref" "shared" "cue" "var"
+                    "get" "set" "throw" "as" "using" "namespace" "of" "stop" "out" "branch" "on"
+                    "pro" "inherits" "protected" "final" "override" "public" "constant" "invariant"
+                    "implies") 'words)
      1 font-lock-keyword-face)
-    ;; C and Python types (highlight as builtins)
-    (,(regexp-opt '("String") 'words)
+
+    (,(regexp-opt '("String" "nil" "decimal" "number" "dynamic") 'words)
      1 font-lock-builtin-face)
+
     )
   "Additional font lock keywords for Cobra mode.")
 
 (define-derived-mode cobra-mode python-mode "Cobra"
-  "Major mode for Cobra development, derived from Python mode.
-
-\\{cobra-mode-map}"
+  "Major mode for Cobra development, derived from Python mode."
   (setcar font-lock-defaults
           (append python-font-lock-keywords cobra-font-lock-keywords))
-  (set (make-local-variable 'compile-command)
-       (concat "cobra -c " buffer-file-name))
-  (add-to-list (make-local-variable 'compilation-finish-functions)
-               'cobra-compilation-finish)
 )
 
 (provide 'cobra-mode)
